a2200ac44d8ea4e7fd4e5850345cc9f73c65707b
Wita Santos <witasrocha@gmail.com>
2024-05-14 12:57:39 -0300

    v. kickoff

---
+++ myreactapp/src/App.js
@@ -0,0 +1,175 @@
+import logo from './logo.svg';
+//import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+function App() {
+  return (
+     <div>
+            
+
+
+            <nav class="navbar background">
+                <ul class="nav-list">
+                    <div class="logo">
+ //                       <img src=
+//"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS8i30uIxEQpkiYmZYLZ2c2xbB3sO9Z4U50AeC4kqT&s"
+   //                     />
+                    </div>
+                                
+                </ul>
+ 
+                <div class="rightNav">
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                    />
+                    <button class="btn btn-sm">
+                        Search
+                    </button>
+                </div>
+            </nav>
+ 
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>            
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>     
+    </div>
+  </div>
+</nav>
+
+
+            <section class="section">
+                <div class="box-main">
+                    <div class="firstHalf">
+                        <h1 class="text-big">
+                            Programação para Web
+                        </h1>
+                        <p class="text-small">
+                            Hunting down a relevant job
+                            requires proper techniques for
+                            showcasing your potential to the
+                            employer. But with the advent of
+                            COVID-19, it has become a bit
+                            challenging and competitive to
+                            reach out for your dream job.
+                            Many individuals have lost their
+                            jobs during these times, and on
+                            the other hand, freshers are
+                            facing difficulties while
+                            applying for a new job. But
+                            there is no need for panic, you
+                            can change your ways and
+                            streamline things in a way that
+                            you get a proper result.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            Usando o Github
+                        </h1>
+                        <p class="text-small">
+                            Um sistema de controle de versão, ou VCS, monitora o histórico de alteraçoes à medida que as pessoas e equipes colaboram em projetos em conjunto. Como os desenvolvedores fazem alterações no projeto, qualquer versão anterior do projeto pode ser recuperada a qualquer momento.
+
+                              Os desenvolvedores podem revisar o histórico do projeto para descobrir:
+
+                              Quais alterações foram feitas?
+                              Quem fez as alterações?
+                              Quando as alterações foram feitas?
+                              Por que as alterações foram necessárias?
+                              Os VCSs fornecem a cada colaborador uma visão unificada e consistente de um projeto, evidenciando o trabalho que já está em andamento. Ver um histórico transparente das alterações, quem as fez, e como eles contribuem para o desenvolvimento de um projeto ajuda os integrantes da equipe a manter-se alinhados enquanto trabalham de forma independente.
+
+                              Em um sistema de controle de versão distribuído, cada desenvolvedor tem uma cópia completa do projeto e do histórico do projeto. Ao contrário dos sistemas de controle de versão centralizados conhecidos, os DVCSs não precisam de uma conexão constante com um repositório central. Git é o sistema de controle de versão distribuída mais popular. O Git é comumente usado para o desenvolvimento de software de código aberto e comercial, com benefícios significativos para indivíduos, equipes e negócios.
+
+                              O Git permite que os desenvolvedores vejam toda a linha do tempo das suas alterações, decisões e progressão de qualquer projeto em um só lugar. Desde o momento em que acessam a história de um projeto, o desenvolvedor tem todo o contexto de que precisa para entender e começar a contribuir.
+
+                              Os desenvolvedores trabalham em todos os fusos horários. Com um DVCS como o Git, a colaboração pode acontecer a qualquer momento enquanto mantém a integridade do código-fonte. Ao usar branches, os desenvolvedores podem propor alterações no código de produção.
+
+                              As empresas que usam o Git podem derrubar as barreiras de comunicação entre equipes e mantê-las focadas em fazer o melhor trabalho. Além disso, o Git possibilita alinhar especialistas em todos os negócios para colaborar em grandes projetos.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            HTML e CSS (Boostrap)
+                        </h1>
+                        <p class="text-small">
+                            Bootstrap é um framework front-end que fornece estruturas de CSS para a criação de sites e aplicações responsivas de forma rápida e simples. Além disso, pode lidar com sites de desktop e páginas de dispositivos móveis da mesma forma.
+
+                              Originalmente, o Bootstrap foi desenvolvido para o Twitter por um grupo de desenvolvedores liderados por Mark Otto e Jacob Thornton Logo e se tornou uma das estruturas de front-end e projetos de código aberto mais populares do mundo.
+
+                              Antes de ser uma estrutura de código-fonte aberto, o Bootstrap era conhecido como Twitter Blueprint. Após alguns meses de desenvolvimento, o Twitter realizou sua primeira Hack Week: o projeto ganhou uma grande popularidade quando desenvolvedores de todos os níveis de habilidade usaram o framework sem qualquer orientação externa. Após o evento, ele serviu como guia de estilo para o desenvolvimento de ferramentas internas na empresa por mais de um ano antes de seu lançamento se tornar público.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            Usando o React
+                        </h1>
+                        <p class="text-small">
+                            Machine Learning is the field of
+                            study that gives computers the
+                            capability to learn without
+                            being explicitly programmed. ML
+                            is one of the most exciting
+                            technologies that one would have
+                            ever come across. As it is
+                            evident from the name, it gives
+                            the computer that makes it more
+                            similar to humans: The ability
+                            to learn. Machine learning is
+                            actively being used today,
+                            perhaps in many more places than
+                            one would expect.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <footer className="footer">
+                <p className="text-footer">
+                    Copyright ©-Todos os Direitos Reservados
+                </p>
+            </footer>
+        </div>
+  );
+}
+
+export default App;
